027b0c56d267cc4a9f4c4b20d509675ec5dca773
Francine Lucca <francinelucca@hotmail.com>
2022-06-09 17:37:10 -0400

    edit story

--- src/stories/NewStory.stories.tsx
+++ src/stories/NewStory.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-const NewStoryComponent = () => <div>Hiiiii</div>
+const NewStoryComponent = () => <div>Hiiiii - Edit</div>
 
 
 export default {
