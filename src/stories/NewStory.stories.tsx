64194fdc5659fe89d51d42de4bfa8a0c1cc02fe8
Francine Lucca <francinelucca@hotmail.com>
2022-06-09 17:45:09 -0400

    inside of separate branch

--- src/stories/NewStory.stories.tsx
+++ src/stories/NewStory.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-const NewStoryComponent = () => <div>Hiiiii - Edit</div>
+const NewStoryComponent = () => <div>Hiiiii - Edit - Edit 2</div>
 
 
 export default {
