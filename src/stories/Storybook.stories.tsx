2c73b36e67ffe832d7467c3a76694c73fe8cdd80
Francine Lucca <francinelucca@hotmail.com>
2022-06-10 16:10:13 -0400

    add a storybook story

---
+++ src/stories/Storybook.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+const NewStoryComponent = () => <div>This is the storybook story</div>
+
+
+export default {
+  title: 'Example/NewStory',
+  component: NewStoryComponent,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof NewStoryComponent>;
+
+const Template: ComponentStory<any> = (args) => <NewStoryComponent {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
